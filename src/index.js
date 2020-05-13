476e5b407c40f5292755a439283b3b197042203c
lliqi <614124284@qq.com>
2020-05-13 14:06:46 +0800

    添加createStore,并创建themeReducer用来生成store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,36 @@ import PropTypes from "prop-types";
 import "./index.css";
 import Header from "./Header.js";
 import Content from "./Content.js";
+function createStore(reducer) {
+  let state = null;
+  const listeners = [];
+  const getState = () => state;
+  const subScribe = (listener) => listeners.push(listener);
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  };
+  dispatch({});
+  return { getState, dispatch, subScribe };
+}
+const themeReducer = (action) => {
+  if (!state) {
+    return {
+      themeColor: "red",
+    };
+  }
+  switch (action.type) {
+    case "CHANGE_COLOR":
+      return {
+        ...state,
+        themeColor: action.themeColor,
+      };
+      break;
+    default:
+      return state;
+  }
+};
+const store = createStore(themeReducer);
 class Index extends Component {
   render() {
     return (
