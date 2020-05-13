77b3574181f36f871191ab1c522a3f5707b97300
lliqi <614124284@qq.com>
2020-05-13 14:40:04 +0800

    Content.js和ThemeSwitch.js也获取store

--- src/Content.js
+++ src/Content.js
@@ -3,10 +3,27 @@ import PropTypes from "prop-types";
 import ThemeSwitch from "./ThemeSwitch.js";
 
 class Content extends Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+  constructor() {
+    super();
+    this.state = { themeColor: "" };
+  }
+  componentWillMount() {
+    this._updateThemeColor();
+  }
+  _updateThemeColor() {
+    const { store } = this.context;
+    const state = store.getState();
+    this.setState({
+      themeColor: state.themeColor,
+    });
+  }
   render() {
     return (
       <div>
-        <p>React.js小书内容</p>
+        <p style={{ color: this.state.themeColor }}>React.js小书内容</p>
         <ThemeSwitch />
       </div>
     );
--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -1,11 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 class ThemeSwitch extends Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+  constructor() {
+    super();
+    this.state = { themeColor: "" };
+  }
+  componentWillMount() {
+    this._updateThemeColor();
+  }
+  _updateThemeColor() {
+    const { store } = this.context;
+    const state = store.getState();
+    this.setState({
+      themeColor: state.themeColor,
+    });
+  }
   render() {
     return (
       <div>
-        <button>red</button>
-        <button>blue</button>
+        <button style={{ color: this.state.themeColor }}>red</button>
+        <button style={{ color: this.state.themeColor }}>blue</button>
       </div>
     );
   }
